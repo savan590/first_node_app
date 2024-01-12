ad8b04580bf6334e3c3d365bbe599a2db6090190
savan590 <savanpatel590@gmail.com>
2024-01-12 15:50:08 +0530

    first node app

---
+++ index.js
@@ -0,0 +1,167 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+dotenv.config()
+const bcrypt = require('bcrypt')
+
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+app.set('view engine', 'ejs')
+
+
+app.get("/",(req,res)=>{
+        res.json({
+            status : 'success',
+            message : 'welcome to the server'
+         })
+});
+
+app.get('/health', (req, res) => {
+    try{
+    const currentTime = new Date().toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      })
+   
+    res.json({ 
+        serverName : 'The Week List', 
+        time : currentTime , 
+        serverStatus : 'active'
+    });
+    }
+    catch(error){
+        console.log(error)
+        res.json({
+            status : 'fail',
+            message : 'something went wrong Please active your server'
+         })
+    }
+  });
+
+const User = mongoose.model('User', {
+    fullname: String,
+    email: String,
+    password: String,
+    age: Number,
+    gender: String,
+    mobile: String,
+  });
+
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find({})
+      res.json({
+        status: 'SUCCESS',
+        data: users
+      })
+    } catch (error) {
+      res.json({ 
+        status: 'FAILED',
+        message: 'Something went wrong'
+      })
+    }
+  })
+
+  app.post('/signup', async (req, res) => {
+    try {
+      const { fullname, email, password, age, gender,mobile} = req.body
+      const encryptedPassword = await bcrypt.hash(password, 10)
+      await User.create({ fullname, email, password: encryptedPassword, age, gender,mobile })
+      res.json({
+        status: 'SUCCESS',
+        message: "You've signed up successfully!"
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({ 
+        status: 'FAILED',
+        message: 'Something went wrong'
+      })
+    }
+  })
+
+  app.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body
+      const user = await User.findOne({ email })
+      if(user) {
+        let hasPasswordMatched = await bcrypt.compare(password, user.password)
+        if(hasPasswordMatched) {
+          const jwttoken = jwt.sign(user.toJSON(), process.env.JWT_SECRET, { expiresIn: 60*30 })
+          res.json({
+            status: 'SUCCESS',
+            message: "You've logged in successfully!",
+            jwttoken
+          })
+        } else {
+          res.json({ 
+            status: 'FAILED',
+            message: 'Incorrect credentials! Please try again'
+          })
+        }
+      } else {
+        res.json({ 
+          status: 'FAILED',
+          message: 'User does not exist'
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      res.json({ 
+        status: 'FAILED',
+        message: 'Incorrect credentials! Please try again'
+      })
+    }
+  })
+
+// Middleware for authentication and authorization
+const authenticate = (req, res, next) => {
+  // if (!token) {
+  //   return res.status(401).json({ message: 'Unauthorized' });
+  // }
+  
+  try {
+      const {jwttoken}= req.headers;
+      const decoded = jwt.verify(jwttoken,process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Invalid token' });
+    }
+  };
+  
+  // Example of a protected route
+  app.get('/profile', authenticate, async (req, res) => {
+  //   try {
+  //     const user = await User.findById(req.userId);
+  //     if (!user) {
+  //       return res.status(404).json({ message: 'User not found' });
+  //     }
+  
+  //     res.json({ user });
+  //   } catch (error) {
+  //     console.error(error);
+      res.json({ message: 'hi' });
+  //   }
+  });
+  
+  app.listen(process.env.PORT,() =>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log(`Server running on http://localhost:${process.env.PORT}`))
+    .catch(error => console.error(error))
+    
+});
+
+
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
